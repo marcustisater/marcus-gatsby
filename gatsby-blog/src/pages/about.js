0002a25c65d4e3acc93df58de4bdd513a74e1abb
marcus.tisater@happiness.se <marcus.tisater@happiness.se>
2017-07-17 18:02:30 +0200

    organized blog posts

--- gatsby-blog/src/pages/about.js
+++ gatsby-blog/src/pages/about.js
@@ -11,21 +11,19 @@ import boxingImage from "./img/boxing.png"
 const List = styled.ul`
   margin: 0;
   padding: 0;
+  border-top: 1px solid #e5e5e5;
+  margin-top: 2rem;
 `;
 
 const ListItem = styled.li`
-  padding: 2rem 2rem 2rem 0;
+  padding: 1.5rem 0;
   lineHeight: 1.5;
   display: flex;
-  alignItems: center;
-  border-color: rgba(0,0,0,.1);
-  border-bottom-style: solid;
-  border-bottom-width: 1px;
+  align-items: center;
 `;
 
 const ListItemImage = styled.img`
-  width: 5rem;
-  height: 5rem;
+  height: 7rem;
   border-radius: 100%;
 `;
 
@@ -37,7 +35,7 @@ const ListItemDescription = styled.div`
 `;
 
 const ListItemDescriptionTitle = styled.p`
-  font-size: 1.25rem;
+  font-size: 1.35rem;
   margin-bottom: 0;
   margin-top: 0;
 `;
@@ -53,6 +51,7 @@ const HeadLine = styled.h1 `
 const HeadLineText = styled.span `
   display: block;
   font-size: 2rem;
+  margin-top: -5px;
 `
 
 export default class About extends React.Component {
@@ -68,13 +67,13 @@ export default class About extends React.Component {
 
         </HeadLine>
 
-        <p> I'm a front-end developer that loves writing semantic markup and structured CSS. </p>
+        <p> I'm a front-end developer that loves writing semantic markup and structured RWD CSS. </p>
 
-        <p> I have a big heart for open source, worked with people from the PostCSS team, involved within the Swedish WordPress community organzing meetups and camps.  </p>
+        <p>I do a lot of script tooling such as gulp and webpack, also working with MVCs like React and Vue. </p>
 
-        <p>I do a lot of script tooling such as configuring gulp and webpack, also been working with MVCs React and Vue. </p>
+        <p> I ❤️ open source, I have worked with people from the <a href="github.com/postcss/">PostCSS team</a> building great stuff and been part of an community. I'm now actively involved within the Swedish WordPress community, <a href="http://www.meetup.com/WordPress-Stockholm/">organzing meetups</a> and <a href="2017.stockholm.wordcamp.org">WordCamps</a> with great folks!
 
-        <p>Besides coding, I strive to be as athletic as I can be by doing MMA. I also have a two year old bernese mountain dog… that eats a lot of food 😂 </p>
+      </p> I also help maintain <a href="http://wpsv.se"> WordPress Sverige</a> and the <a href="http://sv.wordpress.org">installation language profile</a>.
 
         <List>
 
@@ -84,31 +83,9 @@ export default class About extends React.Component {
               alt={`boxing`}
             />
               <ListItemDescription>
-                <ListItemDescriptionTitle>Training</ListItemDescriptionTitle>
-                <ListItemDescriptionContent>I practice mixed martial arts and </ListItemDescriptionContent>
+                <ListItemDescriptionTitle>Don't forget your health!</ListItemDescriptionTitle>
+                <ListItemDescriptionContent>I practice mixed martial arts, trying to live an active lifestyle. You should too.</ListItemDescriptionContent>
               </ListItemDescription>
-          </ListItem>
-
-          <ListItem>
-            <ListItemImage
-              src={postcssImage}
-              alt={`postcss`}
-            />
-            <ListItemDescription>
-                <ListItemDescriptionTitle>PostCSS</ListItemDescriptionTitle>
-                <ListItemDescriptionContent>I lead the scrum of postcss.org and helped with the development in React. I am trying to help the community grow.</ListItemDescriptionContent>
-            </ListItemDescription>
-          </ListItem>
-
-          <ListItem>
-            <ListItemImage
-              src={wpsvImage}
-              alt={`wordpress sverige`}
-            />
-            <ListItemDescription>
-              <ListItemDescriptionTitle>WordPress Sverige</ListItemDescriptionTitle>
-              <p style={{margin: 0}}>I help organize meetups in Stockholm, also last years WordCamp Stockholm 2016 at Internetdagarna.</p>
-            </ListItemDescription>
           </ListItem>
 
           <ListItem>
@@ -117,11 +94,12 @@ export default class About extends React.Component {
               alt={`speaker`}
             />
             <ListItemDescription>
-                <ListItemDescriptionTitle>Speaker</ListItemDescriptionTitle>
-                <ListItemDescriptionContent>Sometimes I speak at conferences and meetups</ListItemDescriptionContent>
+                <ListItemDescriptionTitle>Occasionall Speaker</ListItemDescriptionTitle>
+                <ListItemDescriptionContent>Sometimes I speak at conferences and meetups. You should do it as well.</ListItemDescriptionContent>
             </ListItemDescription>
           </ListItem>
         </List>
+
       </div>
     );
   }
